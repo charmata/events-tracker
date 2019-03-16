ec3b329b46193647a9edc99f7bbcb431b41555cd
Chris Armata <chris.armata@gmail.com>
2019-03-16 18:17:57 -0400

    Add Firebase config

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyDiIL4vSQBo1cdD1Lh-rTGllYFUuBt5HGE",
+  authDomain: "events-tracker-7f444.firebaseapp.com",
+  databaseURL: "https://events-tracker-7f444.firebaseio.com",
+  projectId: "events-tracker-7f444",
+  storageBucket: "events-tracker-7f444.appspot.com",
+  messagingSenderId: "567554433256"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
