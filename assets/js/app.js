379fa16d747eac419c731901ace0b5ed32a6157a
Chris Armata <chris.armata@gmail.com>
2019-03-19 20:35:17 -0400

    Parse data from Ticketmaster API

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,47 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
+
+var data = [];
+
+var endpoint = "https://app.ticketmaster.com/discovery/v2/events";
+
+var key = "";
+
+function searchEvents(q) {
+  var queryUrl =
+    endpoint +
+    "?apikey=" +
+    key +
+    "&unit=km&city=Toronto&countryCode=ca&preferredCountry=ca&startDateTime=" +
+    moment().format("YYYY-MM-DDTHH:mm:ssZ") +
+    "&size=5&keyword=" +
+    q;
+
+  $.ajax({
+    url: queryUrl
+  }).then(function(response) {
+    console.log(response);
+
+    response._embedded.events.forEach(event => {
+      var newEvent = {};
+      newEvent.name = event.name;
+      newEvent.link = event.url;
+      newEvent.date = event.dates.start.localDate;
+      newEvent.time = event.dates.start.localTime;
+      newEvent.status = event.dates.status.code;
+      newEvent.venue = event._embedded.venues[0].name;
+      newEvent.latitude = event._embedded.venues[0].location.latitude;
+      newEvent.longitude = event._embedded.venues[0].location.longitude;
+      event.images.forEach(image => {
+        if (image.width === 100) {
+          newEvent.thumb = image.url;
+        }
+      });
+
+      data.push(newEvent);
+    });
+
+    console.log(data);
+  });
+}
