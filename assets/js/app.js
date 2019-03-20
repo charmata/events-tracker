2aab2de1556713935e59f6acd9dafba72c3f5476
Chris Armata <chris.armata@gmail.com>
2019-03-19 21:01:47 -0400

    Store events data by event id

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-var data = [];
+var data = {};
 
 var endpoint = "https://app.ticketmaster.com/discovery/v2/events";
 
@@ -28,25 +28,22 @@ function searchEvents(q) {
   $.ajax({
     url: queryUrl
   }).then(function(response) {
-    console.log(response);
-
     response._embedded.events.forEach(event => {
-      var newEvent = {};
-      newEvent.name = event.name;
-      newEvent.link = event.url;
-      newEvent.date = event.dates.start.localDate;
-      newEvent.time = event.dates.start.localTime;
-      newEvent.status = event.dates.status.code;
-      newEvent.venue = event._embedded.venues[0].name;
-      newEvent.latitude = event._embedded.venues[0].location.latitude;
-      newEvent.longitude = event._embedded.venues[0].location.longitude;
+      var id = event.id;
+      data[id] = {};
+      data[id].name = event.name;
+      data[id].link = event.url;
+      data[id].date = event.dates.start.localDate;
+      data[id].time = event.dates.start.localTime;
+      data[id].status = event.dates.status.code;
+      data[id].venue = event._embedded.venues[0].name;
+      data[id].latitude = event._embedded.venues[0].location.latitude;
+      data[id].longitude = event._embedded.venues[0].location.longitude;
       event.images.forEach(image => {
         if (image.width === 100) {
-          newEvent.thumb = image.url;
+          data[id].thumb = image.url;
         }
       });
-
-      data.push(newEvent);
     });
 
     console.log(data);
