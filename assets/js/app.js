362042dc7c52ab382ade2515874fda90b396fb7d
Chris Armata <chris.armata@gmail.com>
2019-03-21 20:15:40 -0400

    Sort by date, add pagination, store price range

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,26 +15,31 @@ var endpoint = "https://app.ticketmaster.com/discovery/v2/events";
 
 var key = "";
 
-function searchEvents(q) {
+function searchEvents(q, p) {
   var queryUrl =
     endpoint +
     "?apikey=" +
     key +
-    "&unit=km&city=Toronto&countryCode=ca&preferredCountry=ca&startDateTime=" +
+    "&unit=km&countryCode=CA&startDateTime=" +
     moment().format("YYYY-MM-DDTHH:mm:ssZ") +
-    "&size=5&keyword=" +
+    "&sort=date,asc&size=5&page=" +
+    p +
+    "&keyword=" +
     q;
 
   $.ajax({
     url: queryUrl
   }).then(function(response) {
+    console.log(response);
     response._embedded.events.forEach(event => {
       var id = event.id;
       data[id] = {};
       data[id].name = event.name;
       data[id].link = event.url;
       data[id].date = event.dates.start.localDate;
-      data[id].time = event.dates.start.localTime;
+      if (!event.dates.start.timeTBA) {
+        data[id].time = event.dates.start.localTime;
+      }
       data[id].status = event.dates.status.code;
       data[id].venue = event._embedded.venues[0].name;
       data[id].latitude = event._embedded.venues[0].location.latitude;
@@ -44,6 +49,10 @@ function searchEvents(q) {
           data[id].thumb = image.url;
         }
       });
+      if (event.priceRanges) {
+        data[id].minPrice = event.priceRanges[0].min;
+        data[id].maxPrice = event.priceRanges[0].max;
+      }
     });
 
     console.log(data);
