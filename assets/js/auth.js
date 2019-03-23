5a3079b08778a3be1dd3f747712656b92e57a0c6
toshyamsundar <toshyamsundar@yahoo.com>
2019-03-23 12:01:45 -0400

    Added signin/signup modal

---
+++ assets/js/auth.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#signin").on("click", function() {
+    $("#modal-signin").modal("show");
+  });
+
+  $("#signup").on("click", function() {
+    $("#modal-signup").modal("show");
+  });
+
+  $("#btn-signin").on("click", function() {});
+});
