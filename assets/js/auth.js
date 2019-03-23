d66e4e285b04601277a701114ba5fcf5d614b4f6
toshyamsundar <toshyamsundar@yahoo.com>
2019-03-23 14:05:54 -0400

    Added js for signup, collect user details and create firebase user

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -7,5 +7,50 @@ $(document).ready(function() {
     $("#modal-signup").modal("show");
   });
 
-  $("#btn-signin").on("click", function() {});
+  $("#btn-signup").on("click", function() {
+    var userEmail = $("#signup-email")
+      .val()
+      .trim();
+
+    var userPassword = $("#signup-password")
+      .val()
+      .trim();
+
+    var userName = $("#signup-name")
+      .val()
+      .trim();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(userEmail, userPassword)
+      .then(userObj => {
+        console.log(userObj);
+        if (userObj) {
+          var newUser = firebase.auth().currentUser;
+          newUser
+            .updateProfile({
+              displayName: userName
+            })
+            .then(function() {
+              console.log(newUser.displayName);
+              console.log(newUser.uid);
+              $("#modal-signup").modal("hide");
+              $("#signin").hide();
+              $("#signup").hide();
+              $("#signout")
+                .children("a")
+                .text(newUser.displayName);
+              $("#signout").show();
+            })
+            .catch(function(error) {
+              console.log(error);
+            });
+        }
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode + " " + errorMessage);
+      });
+  });
 });
