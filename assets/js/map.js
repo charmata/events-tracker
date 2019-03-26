3a1be93ef1fb91440f668ea3bfde9e4799a12b59
toshyamsundar <toshyamsundar@yahoo.com>
2019-03-26 00:36:05 -0400

    Added popup to markers on the map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,31 +1,68 @@
 $(document).ready(function() {
   var currentUser;
+  var map;
+  var markerCollection = [];
+  var existingLngLat = [];
+  var xOffset = 0;
+  var yOffset = 0;
+
+  var plotMarker = eventSnapshot => {
+    var es = eventSnapshot.val();
+    // console.log(es);
+
+    var eventName = es.name;
+    var eventStatus = es.status;
+    var eventLatitude = es.venue.latitude;
+    var eventLongitude = es.venue.longitude;
+    var eventDate = es.date;
+    var eventTime = es.time;
+
+    var divElem = $("<div>");
+    var h3Elem = $("<h5>" + eventName + "</h5>");
+    var dateElem = $("<p>Date: " + eventDate + " " + eventTime + "</p>");
+    var statusElem = $("<p>Status: " + eventStatus + "</p>");
+
+    $(divElem).append(h3Elem, dateElem, statusElem);
+
+    var popup = new mapboxgl.Popup({ offset: 25, className: "marker-popup" }).setDOMContent(divElem[0]);
+
+    // if (markerCollection.length > 0) {
+    //   markerCollection.forEach(element => {
+    //     console.log(element.getLngLat().toArray());
+    //     console.log("Lng: " + eventLongitude);
+    //     console.log("Lat: " + eventLatitude);
+    //     existingLngLat = element.getLngLat().toArray();
+    //     if (eventLongitude === existingLngLat[0] && eventLatitude === existingLngLat[1]) {
+    //       xOffset = -0.2;
+    //       yOffset = -0.1;
+    //     }
+    //   });
+    // }
+
+    var marker = new mapboxgl.Marker()
+      .setLngLat([eventLongitude, eventLatitude])
+      .setPopup(popup)
+      .addTo(map);
+    // markerCollection.push(marker);
+    // xOffset = 0;
+    // yOffset = 0;
+  };
 
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       currentUser = firebase.auth().currentUser;
-      console.log("Signed in");
-      console.log("User: " + currentUser);
+      // console.log("Signed in");
+      // console.log("User: " + currentUser);
       mapboxgl.accessToken =
         "pk.eyJ1IjoidG9zaHlhbXN1bmRhciIsImEiOiJjanRoazR5M2wwOHU0NDNsZTJmNHRwYjM1In0.9ID0Zv8TuBqLP0r6NYiSSQ";
       var mapDatabaseRef = database.ref("events-tracker/p2K1huQpB0fSMBhho64NdidGn6F3/event-details");
-      var map = new mapboxgl.Map({
+      map = new mapboxgl.Map({
         container: "saved-events-map",
         style: "mapbox://styles/mapbox/streets-v11"
       });
 
       mapDatabaseRef.on("child_added", function(eventSnapshot) {
-        var es = eventSnapshot.val();
-        console.log(es);
-
-        var eventName = es.name;
-        var eventStatus = es.status;
-        var eventLatitude = es.latitude;
-        var eventLongitude = es.longitude;
-        var eventDate = es.date;
-        var eventTime = es.time;
-
-        var marker = new mapboxgl.Marker().setLngLat([eventLongitude, eventLatitude]).addTo(map);
+        plotMarker(eventSnapshot);
       });
 
       map.setZoom(3);
@@ -34,4 +71,8 @@ $(document).ready(function() {
       console.log("Signed Out");
     }
   });
+
+  // $(document).on("click", ".mapboxgl-marker", function() {
+
+  // })
 });
