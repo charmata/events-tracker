2591b90f67ec8261105a45bf0d16800650279c59
Chris Armata <chris.armata@gmail.com>
2019-03-31 16:44:03 -0400

    Store additional venue info

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,8 +90,12 @@ function parseData(response) {
           date: event.dates.start.localDate,
           status: event.dates.status.code,
           venue: {
+            id: event._embedded.venues[0].id,
             name: event._embedded.venues[0].name,
-            city: event._embedded.venues[0].city.name
+            city: event._embedded.venues[0].city.name,
+            address: event._embedded.venues[0].address.line1,
+            postalCode: event._embedded.venues[0].postalCode,
+            stateCode: event._embedded.venues[0].state.stateCode
           }
         };
 
@@ -100,7 +104,7 @@ function parseData(response) {
           data[id].time = event.dates.start.localTime;
         }
 
-        // Look for image which is exactly 100px
+        // Look for event image which is exactly 100px
         event.images.forEach(image => {
           if (image.width === 100) {
             data[id].thumb = image.url;
@@ -119,6 +123,13 @@ function parseData(response) {
           data[id].venue.latitude = event._embedded.venues[0].location.latitude;
           data[id].venue.longitude = event._embedded.venues[0].location.longitude;
         }
+
+        // Look for venue image which is exactly 205px
+        event._embedded.venues[0].images.forEach(image => {
+          if (image.width === 205) {
+            data[id].venue.thumb = image.url;
+          }
+        });
       }
       addSearchRow(id);
     });
